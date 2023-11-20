d9edf17fe59a1030748cff344e76e8247fd1e9d2
azambrao02 <a.zambrano02@ufromail.cl>
2023-11-20 14:06:00 -0300

    llene el codigo y los constructores

--- src/main/java/model/Administrativo.java
+++ src/main/java/model/Administrativo.java
@@ -1,17 +1,23 @@
 package model;
 
+import java.util.Date;
+
 public class Administrativo extends Trabajador {
 	private Departamento departamento;
 
-	public String toString() {
-		throw new UnsupportedOperationException();
+	public Departamento getDepartamento(){
+		return this.departamento;
+	}
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
 	}
 
-	public Administrativo(String tituloProfesional, String nombre, String direccion, String estadoCivil, String rut, Date horario) {
-		throw new UnsupportedOperationException();
+	public Administrativo(String tituloProfesional, String nombre, String direccion, String estadoCivil, String rut, Date horario, Departamento departamento) {
+		super(tituloProfesional,nombre,direccion,estadoCivil,rut,horario);
+		this.departamento = departamento;
 	}
 
 	public String getTipo() {
-		throw new UnsupportedOperationException();
+		return "Administrativo";
 	}
 }
--- src/main/java/model/Trabajador.java
+++ src/main/java/model/Trabajador.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Date;
+
 public abstract class Trabajador {
 	private String tituloProfesional;
 	private String nombre;
@@ -12,47 +14,36 @@ public abstract class Trabajador {
 	public String getTituloProfesional() {
 		return this.tituloProfesional;
 	}
-
 	public void setTituloProfesional(String tituloProfesional) {
 		this.tituloProfesional = tituloProfesional;
 	}
-
 	public String getNombre() {
 		return this.nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getDireccion() {
 		return this.direccion;
 	}
-
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
-
 	public String getEstadoCivil() {
 		return this.estadoCivil;
 	}
-
 	public void setEstadoCivil(String estadoCivil) {
 		this.estadoCivil = estadoCivil;
 	}
-
 	public String getRut() {
 		return this.rut;
 	}
-
 	public void setRut(String rut) {
 		this.rut = rut;
 	}
-
 	public Date getHorario() {
 		return this.horario;
 	}
-
 	public void setHorario(Date horario) {
 		this.horario = horario;
 	}
@@ -60,6 +51,11 @@ public abstract class Trabajador {
 	public abstract String getTipo();
 
 	public Trabajador(String tituloProfesional, String nombre, String direccion, String estadoCivil, String rut, Date horario) {
-		throw new UnsupportedOperationException();
+		this.tituloProfesional = tituloProfesional;
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.estadoCivil = estadoCivil;
+		this.rut = rut;
+		this.horario = horario;
 	}
 }
